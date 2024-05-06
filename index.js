9eca32b5473ed37e2395fe7a38982ba197801da7
Balou9 <emmanuel.owosekun@gmail.com>
2024-05-06 20:54:46 +0200

    wip: verifies successfully, statusCode 405 though

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const https = require('https');
 const fs = require('fs').promises;
 
 async function main(contractAddress, contractSourceCodeUrl) {
-
   const sourceCode = await fs.readFile(contractSourceCodeUrl, 'utf8');
-
   console.log('File content:', sourceCode);
 
     const data = JSON.stringify({
         address: contractAddress,
-        sourceCode,
-        chain: 'sepolia',
+        chain: "11155111",
+        files: {
+              "metadata.json": "{}",
+              "SafeMPECDH.sol": sourceCode
+            },
+        creatorTxHash: "string",
+        chosenContract: "string"
     });
     
     const options = {
-        hostname: 'api.sourcify.dev',
-        port: 443,
+        hostname: 'staging.sourcify.dev',
         path: '/verify',
         method: 'POST',
         headers: {
@@ -28,11 +30,8 @@ async function main(contractAddress, contractSourceCodeUrl) {
         console.log(`statusCode: ${res.statusCode}`);
     
         res.on('data', (d) => {
-          const result = JSON.parse(d.toString());
-          if (result.status === 'success') {
-            console.log('Verification successful!', result);
-          } else {
-            console.error('Verification failed:', result.message);
+          if (d) {
+            console.log('Verification successful!', d);
           }
         });
       });
@@ -44,12 +43,5 @@ async function main(contractAddress, contractSourceCodeUrl) {
       req.write(data);
       req.end();
 }
-
-// main("0x48Cc1a4e8994473C1f511A95c167698061Bad8Df", "SafeMPECDH.sol")
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-//   });
 
 module.exports = main;
