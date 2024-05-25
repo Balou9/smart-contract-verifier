1a18100d5e99ab1bc7d13876fc95c58346157f12
Balou9 <emmanuel.owosekun@gmail.com>
2024-05-25 22:49:04 +0200

    feat: enables github source code smart contract verification

--- index.js
+++ index.js
@@ -2,58 +2,59 @@ const https = require('https');
 const fs = require('fs').promises;
 
 async function main(contractAddress, contractSourceCodeUrl) {
-  const sourceCode = await fs.readFile(contractSourceCodeUrl, 'utf8');
-  // console.log('File content:', sourceCode);
+  if (contractSourceCodeUrl.startsWith("https://raw.githubusercontent.com/")) {
+    var sourceCode = await fetch(contractSourceCodeUrl)
+      .then((response) => response.text());
+  } else {
+    var sourceCode = await fs.readFile(contractSourceCodeUrl, 'utf8');
+  }
 
-    const data = JSON.stringify({
-        address: contractAddress,
-        chain: "11155111",
-        files: {
-              "metadata.json": "{}",
-              "SafeMPECDH.sol": sourceCode
-            },
-        creatorTxHash: "string",
-        chosenContract: "string"
+  const data = JSON.stringify({
+      address: contractAddress,
+      chain: "11155111",
+      files: {
+            "metadata.json": "{}",
+            "SafeMPECDH.sol": sourceCode
+          },
+      creatorTxHash: "string",
+      chosenContract: "string"
+  });
+  
+  const options = {
+      hostname: 'staging.sourcify.dev',
+      path: '/verify',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      },
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+    let responseBody = '';
+    res.on('data', (d) => {
+      responseBody += d.toString();
     });
-    
-    const options = {
-        hostname: 'staging.sourcify.dev',
-        path: '/verify',
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': data.length,
-        },
-    };
-
-    return new Promise((resolve, reject) => {
-      const req = https.request(options, (res) => {
-        console.log(`statusCode: ${res.statusCode}`);
-
-        let responseBody = '';
-        res.on('data', (d) => {
-          responseBody += d.toString();
-        });
-
-        res.on('end', () => {
-          try {
-            if (res.statusCode === 200) {
-              // const response = JSON.parse(responseBody);
-              resolve(responseBody)
-            } else {
-              reject(new Error(`Verification failed: ${responseBody}`))
-            }
-          } catch (error) {
-            reject(error)
-          }
-      });
+    res.on('end', () => {
+      try {
+        if (res.statusCode === 200) {
+          // const response = JSON.parse(responseBody);
+          resolve(responseBody)
+        } else {
+          reject(new Error(`Verification failed: ${responseBody}`))
+        }
+      } catch (error) {
+        reject(error)
+      }
     });
+  });
 
-    req.on('error', reject);
-
-    req.write(data);
-    req.end();
-    });
+  req.on('error', reject);
+  req.write(data);
+  req.end();
+  });
 }
 
 module.exports = main;
